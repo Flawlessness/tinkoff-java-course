8090e8d9619e63a8938164ab35bea92d7a354587
Dmitry <dimonchikjot874@gmail.com>
2023-11-18 02:27:52 +0700

    minor changes

--- src/test/java/edu/hw6/task5/HackerNewsTest.java
+++ src/test/java/edu/hw6/task5/HackerNewsTest.java
@@ -1,15 +1,9 @@
 package edu.hw6.task5;
 
 import org.junit.jupiter.api.Test;
-import java.util.Arrays;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 public class HackerNewsTest {
-
-    private static final long ID = 38249214;
-    private static final String EXPECTED_TOPIC
-        = "Ship Shape";
 
     @Test
     void testHackerNews() {
@@ -17,16 +11,14 @@ public class HackerNewsTest {
         HackerNews hackerNews = new HackerNews();
 
         long[] IDs = hackerNews.hackerNewsTopStories();
-        String topic = hackerNews.news(ID);
 
-        assertFalse(
-            Arrays.stream(IDs).anyMatch(v -> v == ID)
+        assertTrue(
+            IDs.length > 0
         );
 
-        assertEquals(
-            EXPECTED_TOPIC,
-            topic
-        );
+        assertDoesNotThrow(() -> {
+            String topic = hackerNews.news(IDs[0]);
+        });
 
     }
 }
