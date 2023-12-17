97e910f35583e30f8a7a05ac6d0c26e0b368cf30
Dmitry <dimonchikjot874@gmail.com>
2023-12-18 01:37:33 +0700

    fixes

--- src/test/java/edu/hw11/task3/DynamicFibCalculatorClassTest.java
+++ src/test/java/edu/hw11/task3/DynamicFibCalculatorClassTest.java
@@ -49,7 +49,6 @@ public class DynamicFibCalculatorClassTest {
 
                         //return getFib(n - 1) + getFib(n - 2);
                         mv.visitLabel(l1);
-                        mv.visitFrame(Opcodes.F_SAME, 0, null, 0, null);
                         mv.visitVarInsn(Opcodes.ALOAD, 0);
                         mv.visitVarInsn(Opcodes.ILOAD, 1);
                         mv.visitInsn(Opcodes.ICONST_1);
